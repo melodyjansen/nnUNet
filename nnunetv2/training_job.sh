6d958554cc45afffb1d9ffb6d4d8c153c6c31661
Melody Jansen <65301579+melodyjansen@users.noreply.github.com>
2025-05-30 19:57:39 +0200

    Create training_job.sh

---
+++ nnunetv2/training_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=csedu
+#SBATCH --account=cseduIMC037
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --time=9:00:00
+#SBATCH --output=logs/train-%j.out
+#SBATCH --error=logs/train-%j.err
+
+# Activate your virtual environment
+source /scratch/fvewijk/venv/nnunet_env/bin/activate
+export nnUNet_raw="nnUNet_raw"
+export nnUNet_preprocessed="nnUNet_preprocessed"
+export nnUNet_results="nnUNet_results"
+
+# Start training: adjust these values to match your use case
+nnUNetv2_train 100 3d_fullres 0
+# nnUNetv2_train 100 3d_fullres 0 -tr nnUNetTrainerFocalTversky
